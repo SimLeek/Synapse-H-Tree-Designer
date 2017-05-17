f4cd0cb769176698f43157e47732ea268f41b446
Josh Miklos <josh.miklos@gmail.com>
2017-05-16 17:16:27 -0700

    Better plan for dealing with duplicates efficiently.

--- gaussian-pick.py
+++ gaussian-pick.py
@@ -7,13 +7,7 @@ def generate_neuron_input_points(center_x, center_y, sigma_x, sigma_y, max_width
     """Creates a gaussian distribution of points that a neuron can get input from, limited to rectangular bounds."""
     #todo: warn on large sigmas, as they'll cause many repeated loops, and error when max_height/width is 0
     #todo: can't pick same points, need to use a-star algorithm to find nearest open space
-    # Breadth first search is the best method to fix this, with the goal being any open space,
-    #  and building and keeping the shortest path tree
-    #  with the nearest open space overriding other goals for each point
-    #  Also, store the goal point as it's own class and add points in b-tree to that class when they say it's their goal
-    #   and move points bordering 8 others (2D) in same class to non-bordering index group in b-tree
-    #   because when that open space goal disappears, the points bordering those can be selected for and
-    #   the new nearest goal can be calculated from those neighbors and cascaded down into the group
+    # Use b-tree, start with all open spots as points in b-tree, search for nearest open spot on collision fail
 
     input_points = []
     for i in range(num_synapses):
